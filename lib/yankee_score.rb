6102d75f6171fd45642772272a4a1e7e3a09a30a
Shmuwol <shmullywolfson@gmail.com>
2016-04-12 21:36:56 -0400

    require 'active_support'

--- lib/yankee_score.rb
+++ lib/yankee_score.rb
@@ -2,11 +2,14 @@ require 'json'
 require 'net/http'
 require 'uri'
 require 'date'
+require 'active_support/core_ext/integer/inflections'
 
 require "yankee_score/version"
 require 'yankee_score/cli'
 require 'yankee_score/game'
 require 'yankee_score/score_scraper'
-re
+require 'yankee_score/team'
+
+
 module YankeeScore
 end
